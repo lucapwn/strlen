4f4456a3d42bb4089b8fc2390c315d1ec81fc36d
Lucas Araújo <luscafter@gmail.com>
2022-02-12 16:44:25 -0300

    Add files via upload

---
+++ strlen.asm
@@ -0,0 +1,37 @@
+; nasm -f elf strlen.asm
+; ld -m elf_i386 -s -o strlen strlen.o
+; ./strlen 
+
+section .data
+	message db "Kernelzera", 0xA
+
+section .text
+	global _start
+
+strlen:
+	mov ebx, 0
+
+next:
+	cmp byte [esi + ebx], 0
+	je end
+
+	inc ebx
+	jmp next
+
+end:
+	mov eax, ebx
+	ret
+
+_start:
+	mov esi, message
+	call strlen
+
+	mov edx, eax
+	mov ecx, message
+	mov ebx, 1
+	mov eax, 4
+	int 80h
+
+	mov eax, 1
+	mov ebx, 0
+	int 80h
